903c43a9df278539d3e33b0447e4adb278dae784
Arush Baluni <128829308+arushbaluni@users.noreply.github.com>
2025-04-06 22:40:47 +0530

    Update Llama_4_Scout.py

--- Llama_4_Scout.py
+++ Llama_4_Scout.py
@@ -9,7 +9,7 @@ Bot_token = os.getenv("BOT_TOKEN")
 
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key= os.getenv("API_KEY"),
+  api_key= os.getenv("GROQ_API_KEY"),
 )
 
 # 🧠 Memory store per user
